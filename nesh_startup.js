41f6fa6e7f3e2e05996bc4eef9b550ebf43cd531
Dara Dermody <daradermody@gmail.com>
2019-05-03 12:34:42 +0100

    Removed not very useful stuff

--- nesh_startup.js
+++
@@ -1,7 +0,0 @@
-const moment = require('moment');
-const fs = require('fs');
-const _ = require('lodash');
-const axios = require('axios');
-
-const es = require('elasticsearch');
-const client = new es.Client({host: 'https://sirenadmin:password@localhost:9220'})
