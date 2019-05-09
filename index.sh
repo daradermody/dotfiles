9030425d730c2474cf45aaed4bf69ac05e222e82
Dara Dermody <daradermody@gmail.com>
2019-05-09 11:29:43 +0100

    Fix sourcing from index

--- index.sh
+++ index.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
 
-source ${RC_DIR}/aliases
-source ${RC_DIR}/environment_variables
-source ${RC_DIR}/functions
-source ${RC_DIR}/shell_settings
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source ${CURRENT_DIR}/aliases
+source ${CURRENT_DIR}/environment_variables
+source ${CURRENT_DIR}/functions
+source ${CURRENT_DIR}/shell_settings
 
